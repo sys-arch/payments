1f9002bbab6cbef56453b6ede2194fff39849348
‎WM <91469450+sys-arch@users.noreply.github.com>
2025-03-10 13:28:59 +0100

    Arreglos

--- src/main/java/edu/uclm/esi/payments/http/UserCreditsController.java
+++ src/main/java/edu/uclm/esi/payments/http/UserCreditsController.java
@@ -1,7 +1,7 @@
 package edu.uclm.esi.payments.http;
 
 import edu.uclm.esi.payments.model.UserCredits;
-import edu.uclm.esi.payments.services.UserCreditsService;
+import edu.uclm.esi.payments.service.UserCreditsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
